a615db1649d49036b87c50c4e14b7b70c68b29d4
ImmortalLegion <127948881+ImmortalLegion@users.noreply.github.com>
2023-03-15 15:31:59 +0200

    Add files via upload

--- usb_dokhelper.lua
+++ usb_dokhelper.lua
@@ -12,18 +12,6 @@ local sampev = require "lib.samp.events"
 local _, myid
 local mynickname
 
-update_state = false
-
-local script_vers = 2
-local script_vers_text = "1.02"
-
-local update_url = "https://raw.githubusercontent.com/ImmortalLegion/usb_dh/main/update.ini"
-local update_path = getWorkingDirectory() .. "/usb_dokhelper_update.ini"
-
-local script_url = "https://raw.githubusercontent.com/ImmortalLegion/usb_dh/main/usb_dokhelper.lua"
-local script_path = thisScript().path
-local updateIni = inicfg.load(nil, update_path)
-
 local nicks = {
 	 -- Commanders
 	 ['Renya_Stoun'] = 'R-S-7',
@@ -36,7 +24,6 @@ local nicks = {
 	 -- Operative
 	 ['Frederick_Wertheim'] = 'F-W-69',
 	 -- Trainee
-	 ['Czar_Stolbentsov'] = 'C-S-21',
 	 ['Yukio_Matsui'] = 'Y-M-88',
 	 ['Looneyz_Kotov'] = 'L-K-34'
 }
@@ -45,20 +32,27 @@ function main()
 	if not isSampLoaded() or not isSampfuncsLoaded() then return end												-- Проверяем загружен ли sampfuncs и SAMP если не загружены - возвращаемся к началу
 	while not isSampAvailable() do wait(100) end																	-- Проверяем загружен ли SA-MP
 
+	
+	update_state = false
+
+	local script_vers = 3
+	local script_vers_text = "2.03"
+
+	local update_url = "https://raw.githubusercontent.com/ImmortalLegion/usb_dh/main/update.ini"
+	local update_path = getWorkingDirectory() .. "/usb_dokhelper_update.ini"
+
+	local script_url = "https://raw.githubusercontent.com/ImmortalLegion/usb_dh/main/usb_dokhelper.lua"
+	local script_path = thisScript().path
+
 	sampAddChatMessage('{333366} «USB Doklad Helper» {808080}успешно загружен. Версия: ' .. script_vers_text, 0xFFFFFF)				    -- Сообщаем об загрузке скрипта
 
 	sampRegisterChatCommand('dk', cmd_dk)																			-- Регистрация команды
 	sampRegisterChatCommand('dn', cmd_dn)                                                         
 	sampRegisterChatCommand('kc', cmd_kc)     
+	   
+	sampRegisterChatCommand('post', cmd_post)      
 	
-	sampRegisterChatCommand('spost', cmd_spost)                                                          
-	sampRegisterChatCommand('ppost', cmd_ppost)                                                          
-	sampRegisterChatCommand('npost', cmd_npost)                                                          
-	sampRegisterChatCommand('epost', cmd_epost)      
-	
-	sampRegisterChatCommand('sdd', cmd_sdd)  
 	sampRegisterChatCommand('cdd', cmd_cdd)  
-	sampRegisterChatCommand('edd', cmd_edd)  
 
 	sampRegisterChatCommand('dh_info', cmd_dh_info)  
 
@@ -69,13 +63,14 @@ function main()
 
 	downloadUrlToFile(update_url, update_path, function(id, status)
 		if status == dlstatus.STATUS_ENDDOWNLOADDATA then 
+			updateIni = inicfg.load(nil, update_path)
 			if tonumber(updateIni.info.vers) > script_vers then
 				sampAddChatMessage('{333366} USB DH info | {808080}Есть обновление. Новая версия: ' .. updateIni.info.vers_text, 0xFFFFFF)	
 				update_state = true
 			else
 				sampAddChatMessage('{333366} USB DH info | {808080}Обновлений нет. Загружена последняя версия: ' .. script_vers_text, 0xFFFFFF)	
 			end
-			os.remove(update_path)
+			--os.remove(update_path)
 		end
 	end)
 
@@ -172,108 +167,30 @@ function cmd_kc(args)
 	end
 end
 
-function cmd_spost(args)
+function cmd_post(args)
+	arg1, arg2 = string.match(args, "(.+) (.+)")
 	local mycod = nicks[mynickname]
-	if #args ~= 0 then
+	if arg1 == nil or arg1 == '' then
+		sampAddChatMessage('{333366} USB DH info | {808080}Вы неправильно ввели аргументы, смотрите /dh_info', 0xFFFFFF)
+	else
 		if mycod ~= nil then
 			local h = os.date()
-			sampProcessChatInput(string.format('/kk %s, заступил в пост-наряд. Сектор: %s. Время: '..os.date('%H:%M'), mycod, args))
-		else
-			sampAddChatMessage('{333366} USB DH info | {808080}Ваш код в таблице сотрудников не обнаружен', 0xFFFFFF)
-			return
-		end
-	else
-		sampAddChatMessage('{333366} USB DH info | {808080}Введите квадрат', 0xFFFFFF)
-		return
-	end
-end
-
-function cmd_ppost(args)
-	local mycod = nicks[mynickname]
-	if #args ~= 0 then
-		if mycod ~= nil then
-			local h = os.date()
-			sampProcessChatInput(string.format('/kk %s, пост-наряд. Перемещаюсь в сектор: %s. Время: '..os.date('%H:%M'), mycod, args))
-		else
-			sampAddChatMessage('{333366} USB DH info | {808080}Ваш код в таблице сотрудников не обнаружен', 0xFFFFFF)
-			return
-		end
-	else
-		sampAddChatMessage('{333366} USB DH info | {808080}Введите квадрат', 0xFFFFFF)
-		return
-	end
-end
-
-function cmd_npost(args)
-	local mycod = nicks[mynickname]
-	if #args ~= 0 then
-		if mycod ~= nil then
-			local h = os.date()
-			sampProcessChatInput(string.format('/kk %s, пост-наряд. Сектор: %s. Время: '..os.date('%H:%M'), mycod, args))
-		else
-			sampAddChatMessage('{333366} USB DH info | {808080}Ваш код в таблице сотрудников не обнаружен', 0xFFFFFF)
-			return
-		end
-	else
-		sampAddChatMessage('{333366} USB DH info | {808080}Введите квадрат', 0xFFFFFF)
-		return
-	end
-end
-
-function cmd_epost(args)
-	local mycod = nicks[mynickname]
-	if #args ~= 0 then
-		if mycod ~= nil then
-			local h = os.date()
-			sampProcessChatInput(string.format('/kk %s, заканчиваю пост-наряд. Сектор: %s. Время: '..os.date('%H:%M'), mycod, args))
-		else
-			sampAddChatMessage('{333366} USB DH info | {808080}Ваш код в таблице сотрудников не обнаружен', 0xFFFFFF)
-			return
-		end
-	else
-		sampAddChatMessage('{333366} USB DH info | {808080}Введите квадрат', 0xFFFFFF)
-		return
-	end
-end
-
-function cmd_sdd()
-	local info = {}
-	if isCharInAnyCar(PLAYER_PED) then
-		local mycar = storeCarCharIsInNoSave(PLAYER_PED)
-		for i = 0, 999 do
-			if sampIsPlayerConnected(i) then
-				local ichar = select(2, sampGetCharHandleBySampPlayerId(i))
-				if doesCharExist(ichar) then
-					if isCharInAnyCar(ichar) then
-						local icar = storeCarCharIsInNoSave(ichar)
-						if mycar == icar then
-							local nicktoid = sampGetPlayerNickname(i)
-							if nicks[nicktoid] ~= nil then
-								local call = nicks[nicktoid]
-								table.insert(info, call)
-							else
-								local nick = string.gsub(sampGetPlayerNickname(i), "(%u+)%l+_(%w+)", "%1.%2")
-								table.insert(info, nick)
-							end
-						end
-					end
-				end
+			if arg1 == 's' then
+				sampProcessChatInput(string.format('/kk %s, заступил в пост-наряд. Сектор: %s. Время: '..os.date('%H:%M'), mycod, arg2))
+			elseif arg1 == 'p' then
+				sampProcessChatInput(string.format('/kk %s, пост-наряд. Перемещаюсь в сектор: %s. Время: '..os.date('%H:%M'), mycod, arg2))
+			elseif arg1 == 'd' then
+				sampProcessChatInput(string.format('/kk %s, пост-наряд. Сектор: %s. Время: '..os.date('%H:%M'), mycod, arg2))
+			elseif arg1 == 'e' then
+				sampProcessChatInput(string.format('/kk %s, заканчиваю пост-наряд. Сектор: %s. Время: '..os.date('%H:%M'), mycod, arg2))
+			else 
+				sampAddChatMessage('{333366} USB DH info | {808080}Вы ввели неправильный модификатор', 0xFFFFFF)
+				return
 			end
-		end
-		local mycod = nicks[mynickname]
-		if mycod ~= nil then
-			if #info > 0 then
-				sampProcessChatInput(string.format('/kk %s, start checking dangerous districts. Working with %s', mycod, table.concat(info,', ')))
-			else
-				sampProcessChatInput(string.format('/kk %s, start checking dangerous districts. Working solo', mycod, args))
-			end
-		else 
-			sampAddChatMessage('{333366} USB DH info | {808080}Ваш код в таблице сотрудников не обнаружен', 0xFFFFFF)		
+		else
+			sampAddChatMessage('{333366} USB DH info | {808080}Ваш код в таблице сотрудников не обнаружен', 0xFFFFFF)
 			return
 		end
-	else 
-		sampAddChatMessage('{333366} USB DH info | {808080}Вы не сидите в автомобиле', 0xFFFFFF)
-		return
 	end
 end
 
@@ -304,58 +221,31 @@ function cmd_cdd(args)
 			end
 			local mycod = nicks[mynickname]
 			if mycod ~= nil then
-				if #info > 0 then
-					sampProcessChatInput(string.format('/kk %s, checking dangerous districts, %s. Working with %s', mycod, args, table.concat(info,', ')))
+				if args == 's' then
+					if #info > 0 then
+						sampProcessChatInput(string.format('/kk %s, start checking dangerous districts. Working with %s', mycod, table.concat(info,', ')))
+					else
+						sampProcessChatInput(string.format('/kk %s, start checking dangerous districts. Working solo', mycod, args))
+					end
+				elseif args == 'e' then
+					if #info > 0 then
+						sampProcessChatInput(string.format('/kk %s, end checking dangerous districts. Working with %s', mycod, table.concat(info,', ')))
+					else
+						sampProcessChatInput(string.format('/kk %s, end checking dangerous districts. Working solo', mycod, args))
+					end
 				else
-					sampProcessChatInput(string.format('/kk %s, checking dangerous districts, %s. Working solo', mycod, args))
+					if #info > 0 then
+						sampProcessChatInput(string.format('/kk %s, checking dangerous districts, %s. Working with %s', mycod, args, table.concat(info,', ')))
+					else
+						sampProcessChatInput(string.format('/kk %s, checking dangerous districts, %s. Working solo', mycod, args))
+					end
 				end
 			else 
 				sampAddChatMessage('{333366} USB DH info | {808080}Ваш код в таблице сотрудников не обнаружен', 0xFFFFFF)		
 				return
 			end
 		else
-			sampAddChatMessage('{333366} USB DH info | {808080}Введите информацию по проверке', 0xFFFFFF)
-			return
-		end
-	else 
-		sampAddChatMessage('{333366} USB DH info | {808080}Вы не сидите в автомобиле', 0xFFFFFF)
-		return
-	end
-end
-
-function cmd_edd()
-	local info = {}
-	if isCharInAnyCar(PLAYER_PED) then
-		local mycar = storeCarCharIsInNoSave(PLAYER_PED)
-		for i = 0, 999 do
-			if sampIsPlayerConnected(i) then
-				local ichar = select(2, sampGetCharHandleBySampPlayerId(i))
-				if doesCharExist(ichar) then
-					if isCharInAnyCar(ichar) then
-						local icar = storeCarCharIsInNoSave(ichar)
-						if mycar == icar then
-							local nicktoid = sampGetPlayerNickname(i)
-							if nicks[nicktoid] ~= nil then
-								local call = nicks[nicktoid]
-								table.insert(info, call)
-							else
-								local nick = string.gsub(sampGetPlayerNickname(i), "(%u+)%l+_(%w+)", "%1.%2")
-								table.insert(info, nick)
-							end
-						end
-					end
-				end
-			end
-		end
-		local mycod = nicks[mynickname]
-		if mycod ~= nil then
-			if #info > 0 then
-				sampProcessChatInput(string.format('/kk %s, end checking dangerous districts. Working with %s', mycod, table.concat(info,', ')))
-			else
-				sampProcessChatInput(string.format('/kk %s, end checking dangerous districts. Working solo', mycod, args))
-			end
-		else 
-			sampAddChatMessage('{333366} USB DH info | {808080}Ваш код в таблице сотрудников не обнаружен', 0xFFFFFF)		
+			sampAddChatMessage('{333366} USB DH info | {808080}Введите модификатор или информацию о проверке', 0xFFFFFF)
 			return
 		end
 	else 
@@ -369,11 +259,6 @@ function cmd_dh_info()
 	sampAddChatMessage('{333366} USB DH info | {9999CC}/dk - {808080}стандартный доклад с тен-кодом', 0xFFFFFF)	
 	sampAddChatMessage('{333366} USB DH info | {9999CC}/dn - {808080}вывод кода сотрудника в чат', 0xFFFFFF)	
 	sampAddChatMessage('{333366} USB DH info | {9999CC}/kc - {808080}сообщение в /kk с личным кодом', 0xFFFFFF)	
-	sampAddChatMessage('{333366} USB DH info | {9999CC}/spost - {808080}начало пост-наряда', 0xFFFFFF)	
-	sampAddChatMessage('{333366} USB DH info | {9999CC}/ppost - {808080}перемещение в пост-наряде в другой сектор', 0xFFFFFF)	
-	sampAddChatMessage('{333366} USB DH info | {9999CC}/npost - {808080}доклад в пост-наряде', 0xFFFFFF)	
-	sampAddChatMessage('{333366} USB DH info | {9999CC}/epost - {808080}конец пост-наряда', 0xFFFFFF)	
-	sampAddChatMessage('{333366} USB DH info | {9999CC}/sdd - {808080}начало проверки опасных районов', 0xFFFFFF)	
-	sampAddChatMessage('{333366} USB DH info | {9999CC}/cdd - {808080}доклад о проверке опасных районов', 0xFFFFFF)	
-	sampAddChatMessage('{333366} USB DH info | {9999CC}/edd - {808080}конец проверки опасных районов', 0xFFFFFF)	
+	sampAddChatMessage('{333366} USB DH info | {9999CC}/post [s, p, d, e] [kv] - {808080}s - старт, p - перемещение, d - доклад, e - конец, kv - квадрат', 0xFFFFFF)	
+	sampAddChatMessage('{333366} USB DH info | {9999CC}/cdd [s, e, mes] - {808080}s - старт проверки, e - конец, mes - сообщение о проверке', 0xFFFFFF)	
 end
